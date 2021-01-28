0fd81101d74580cfa81dfe62396b9f53344ca03f
Dalton Durst <d@ltondur.st>
2021-01-28 18:04:53 +0000

    Disable GL rendering; it causes corrupted rendering
    This will unfortunately cause WebGL to stop working, but it will cause the browser to be usable again. https://gitlab.com/ubports/community-ports/pinephone/-/issues/249

--- overlay/system/etc/profile.d/disable-webengine-viz.sh
+++ overlay/system/etc/profile.d/disable-webengine-viz.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 # Disable the Viz display compositor; it causes stuttery scrolling
+# Disable GL rendering; it causes corrupted rendering
 # This file is part of pinephone_pinetab
-export QTWEBENGINE_CHROMIUM_FLAGS='--disable-viz-display-compositor'
+export QTWEBENGINE_CHROMIUM_FLAGS='--disable-viz-display-compositor --disable-gpu'
